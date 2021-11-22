e9241e9456c47f345a2cb3d63874353867f2a173
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2021-11-21 19:48:27 -0600

    Use a synced folder instead of copying.

--- build-nanobsd.sh
+++ build-nanobsd.sh
@@ -1,17 +1,13 @@
 #!/bin/sh -ex
 
-# The share that gets setup automatically does not work consistently.
-# Unmount the share and use a symlink so that if the bug get fixed all we need to is remove these two lines and the
-# rest of the script should work without any changes.
-sudo umount /vagrant
-sudo ln -s /home/vagrant/net5501 /vagrant/net5501
+# The share that gets setup automatically does not work consistently, so we use sync folders instead.
 
 # Link kernel config.
-sudo ln -s /vagrant/net5501/NET5501       /usr/src/sys/i386/conf/NET5501
-sudo ln -s /vagrant/net5501/NET5501.hints /usr/src/sys/i386/conf/NET5501.hints
+sudo ln -s /home/vagrant/net5501/NET5501       /usr/src/sys/i386/conf/NET5501
+sudo ln -s /home/vagrant/net5501/NET5501.hints /usr/src/sys/i386/conf/NET5501.hints
 
 # Build NanoBSD image.
-sudo /bin/sh /usr/src/tools/tools/nanobsd/nanobsd.sh -c /vagrant/net5501/net5501.cfg
+sudo /bin/sh /usr/src/tools/tools/nanobsd/nanobsd.sh -c /home/vagrant/net5501/net5501.cfg
 
 # todo: Review build(7)
 # todo: Increase ram backed filesystems.
