898a1a4bf0fc6403785ecea2f2518e0031216f24
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2022-01-02 16:15:54 -0600

    Updated to FreeBSD 12.3. Fixed linefeed in build-nanobsd.sh. Fixed an issue where the provision step would report a failure if there were no FreeBSD updates to install.

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "freebsd/FreeBSD-12.2-RELEASE"
+  config.vm.box = "freebsd/FreeBSD-12.3-RELEASE"
 
   # The FreeBSD image updates at startup, so allow for more time.
   config.vm.boot_timeout = 600
--- setup-environment.sh
+++ setup-environment.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -ex
 
 # Install source distribution.
-fetch -o - ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.2-RELEASE/src.txz | tar -x -J -C / -f -
+fetch -o - ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.3-RELEASE/src.txz | tar -x -J -C / -f -
 
 # System automatically applies updates at first boot, but that is before the source distribution is installed, so
 # we need to update that.
 env PAGER=cat freebsd-update --not-running-from-cron fetch
-freebsd-update install
+if freebsd-update updatesready; then
+    freebsd-update install
+fi;
